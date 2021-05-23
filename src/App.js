d6f6e00f72c11ef3b0e51c4a14fcb90ba327d216
Justin Ochoa <just.och92@gmail.com>
2021-05-23 12:11:57 -0400

    added functionality

--- src/App.js
+++ src/App.js
@@ -32,9 +32,23 @@ function App() {
   ]);
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else return currentSongIndex + 1;
+    });
+  }, [currentSongIndex]);
+
   return (
     <div className="App">
-      <Player song={songs[currentSongIndex]} nextSong={songs[nextSongIndex]} />
+      <Player
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
+      />
     </div>
   );
 }
